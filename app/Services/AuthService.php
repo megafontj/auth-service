25a40ba7d6ffc66235385069c211675f76158e94
Safar Umarov <safarumarov711@gmail.com>
2024-05-17 18:25:17 +0700

    some fix without docker

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -15,7 +15,8 @@ class AuthService
     {
         $user = User::create($data);
 
-        UserRegistered::dispatch(array_merge($data, $user->toArray()))->onQueue('user-registered');
+        // not more docker. Account will be created from api-gateway
+        // UserRegistered::dispatch(array_merge($data, $user->toArray()))->onQueue('user-registered');
 
         return $user;
     }
